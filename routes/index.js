fc3e757b955d0cbbe40036582ded72beb6d2325f
rajiv ranjan <raj123.sah@gmail.com>
2018-07-11 19:52:42 +0530

    about page added and link to both page cleared

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,12 @@ var router = express.Router();
 router.get('/', function (req, res) {
     res.render('index', { title: 'herm: Bringing Women Together' });
 });
+// router.get('/index', function (req, res) {
+//     res.render('index', { title: 'herm: Bringing Women Together' });
+// });
+
+router.get('/about', function (req, res) {
+    res.render('aboutus', { title: 'herm: Bringing Women Together' });
+});
 
 module.exports = router;
